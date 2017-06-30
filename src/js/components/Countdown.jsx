bac99e17ae449f5e75fd5dac2127fe912e0903cc
Chaitya Shah <Chaitya62@gmail.com>
2017-06-30 14:32:35 +0530

    Make the countdown to  countdown

--- src/js/components/Countdown.jsx
+++ src/js/components/Countdown.jsx
@@ -5,8 +5,31 @@ class Countdown extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      seconds: 0
+      seconds: 0,
+      countdownStatus: 'stopped'
     }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  }
+  startTimer() {
+    this.timer = setInterval(() => {
+      var newSeconds = this.state.seconds - 1;
+      this.setState({
+        seconds: (newSeconds >= 0
+          ? newSeconds
+          : 0)
+      });
+      if (newSeconds <= 0) {
+        this.setState({countdownStatus: 'stopped'});
+      }
+    }, 1000);
   }
   render() {
     return (
@@ -18,7 +41,8 @@ class Countdown extends Component {
     );
   }
   handleSetCountdown(seconds) {
-    this.setState({seconds});
+    this.setState({seconds: seconds, countdownStatus: 'started'});
+
   }
 }
 
---
+++ src/js/tests/components/Countdown.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import expect from 'expect';
+import ReactTestUtils from 'react-dom/test-utils'
+import $ from 'jquery/dist/jquery.min.js';
+
+import Countdown from 'Components/Countdown';
+
+describe('Countdown', () => {
+  it('should exits', () => {
+    expect(Countdown).toExist();
+  });
+
+  describe('handleSetCountdown', () => {
+    var countdown = ReactTestUtils.renderIntoDocument(<Countdown/>);
+    it('should set state to started and countdown', (done) => {
+
+      countdown.handleSetCountdown(10);
+
+      expect(countdown.state.seconds).toBe(10);
+      expect(countdown.state.countdownStatus).toBe('started');
+      var timeout = setTimeout(() => {
+        expect(countdown.state.seconds).toBe(9);
+        done();
+      }, 1001);
+
+    });
+
+    it('should not countdown to negative seconds', (done) => {
+      countdown.handleSetCountdown(1);
+      var timeout = setTimeout(() => {
+        expect(countdown.state.seconds).toBe(0);
+        done();
+      }, 3001)
+
+    })
+  })
+});
